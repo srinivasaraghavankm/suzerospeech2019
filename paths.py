f064dad217bcfb0f0e96110fa80715e700cf0154
srinivasaraghavankm <srinivasaraghavankm@gmail.com>
2019-07-06 15:50:15 +0530

    path.py modified

--- paths.py
+++ paths.py
@@ -1,2 +1,3 @@
 buckeye_datadir = "/home/kamperh/endgame/datasets/buckeye/"
-zerospeech2019_datadir = "/home/kamperh/endgame/datasets/zerospeech2019/shared/databases/"
+#zerospeech2019_datadir = "/home/kamperh/endgame/datasets/zerospeech2019/shared/databases/"
+zerospeech2019_datadir = "/home/srinivasa/hdd1/zerospeech2019/shared/databases/"
